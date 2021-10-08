668e4a09c10f601834c63f25308fbe3068d17061
HomePC <jimdac@naver.com>
2021-10-08 16:43:48 +0900

    2021-10-08

--- 20210817 sort(정렬)/삽입정렬.py
+++ 20210817 sort(정렬)/삽입정렬.py
@@ -1,9 +1,9 @@
-array = [7,5,9,0,3,1,6,2,4,8]
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(1,len(array)):
-    for j in range(i,0,-1): # 인덱스 i부터 1까지 1씩 감소하며 반복하는 문법
-        if array[j] < array[j-1]: #한 칸씩 왼쪽으로 이동
-            array[j],array[j-1] = array[j-1],array[j]
-        else: #앞쪽은 이미 정렬이 되어있기 때문에 자기보다 작은 데이터 만나면 그자리에서 멈춤
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):  # 인덱스 i부터 1까지 1씩 감소하며 반복하는 문법
+        if array[j] < array[j - 1]:  # 한 칸씩 왼쪽으로 이동
+            array[j], array[j - 1] = array[j - 1], array[j]
+        else:  # 앞쪽은 이미 정렬이 되어있기 때문에 자기보다 작은 데이터 만나면 그자리에서 멈춤
             break
 print(array)
