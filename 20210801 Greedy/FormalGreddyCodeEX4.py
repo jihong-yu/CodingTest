1ff46aa6b73b5509f401c061f22952b5c44a8499
HomePC <jimdac@naver.com>
2021-11-20 16:37:42 +0900

    2021-11-20

--- 20210801 Greedy/FormalGreddyCodeEX4.py
+++ 20210801 Greedy/FormalGreddyCodeEX4.py
@@ -1,14 +1,14 @@
 N = int(input())
-data = list(map(int,input().split()))
-data.sort() #오름차순 정렬(제일중요)
+data = list(map(int, input().split()))
+data.sort()  # 오름차순 정렬(제일중요)
 
-result = 0 #총 그룹의 수
-count = 0 #현재 그룹에 포함된 모험가의 수
+result = 0  # 총 그룹의 수
+count = 0  # 현재 그룹에 포함된 모험가의 수
 
-for i in data: #공포도를 낮은 것 부터 하나씩 확인하며
-    count += 1 #현재 그룹에 해당 모험가를 포함시키기(현재 그룹의 사람 수)
-    if count >= i: # 현재 그룹에 포함된 모험가의 수가 현재의 공포도 이상이라면, 그룹 결성
-        result +=1 #총 그룹의 수 증가
-        count = 0 #현재 그룹에 포함된 모험가의 수 초기화
+for i in data:  # 공포도를 낮은 것 부터 하나씩 확인하며
+    count += 1  # 현재 그룹에 해당 모험가를 포함시키기(현재 그룹의 사람 수)
+    if count >= i:  # 현재 그룹에 포함된 모험가의 수가 현재의 공포도 이상이라면, 그룹 결성
+        result += 1  # 총 그룹의 수 증가
+        count = 0  # 현재 그룹에 포함된 모험가의 수 초기화
 
 print(result)
--- 20210801 Greedy/FormalGreedyCodeEX2.py
+++ 20210801 Greedy/FormalGreedyCodeEX2.py
@@ -1,20 +1,19 @@
-n,k = map(int, input().split())
+n, k = map(int, input().split())
 
 result = 0
 
 while True:
-    #N이 K로 나누어 떨어지는 수가 될 때까지 빼기
+    # N이 K로 나누어 떨어지는 수가 될 때까지 빼기
     target = (n // k) * k
-    result += (n-target)
+    result += (n - target)
     n = target
-    #N이 K보다 작을 때(더이상 나눌 수 없을 때) 반복문 탈출
-    if n<k:
+    # N이 K보다 작을 때(더이상 나눌 수 없을 때) 반복문 탈출
+    if n < k:
         break
-    #k로 나누기
+    # k로 나누기
     n //= k
     result += 1
 
-#마지막으로 남은 수에 대하여 1씩 빼기
-result += (n-1)
-print("result : ",result)
-
+# 마지막으로 남은 수에 대하여 1씩 빼기
+result += (n - 1)
+print("result : ", result)
