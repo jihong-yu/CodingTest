2b63a5e6e2806b1b36807e3859aaef6044566cb2
HomePC <jimdac@naver.com>
2022-02-16 23:40:32 +0900

    2022-02-16 정수론 개념 정리

--- 정수론-야옹님강의/정수론 개념 코드/소인수 분해.py
+++ 정수론-야옹님강의/정수론 개념 코드/소인수 분해.py
@@ -8,13 +8,13 @@ for i in range(2, N + 1):
         print(i, end=" ")
         A //= i
 print()
-#2. sqrt 활용하기
+# 2. sqrt 활용하기
 # n => a, b, c, d, e, f, g, h , i
 # a*b*c*d*e*f*g*h*i => n
 
-#이 때 소인수가 sqrt N 보다 큰 경우는 많아봐야 1개이거나 거의 없다.
+# 이 때 소인수가 sqrt N 보다 큰 경우는 많아봐야 1개이거나 거의 없다.
 
-X = N #N이 불변해야 i*i > N 이성립
+X = N  # N이 불변해야 i*i > N 이성립
 
 for i in range(2, N + 1):
     if i * i > N:
