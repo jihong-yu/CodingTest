420c0b83e421273d4c7603661acd5a76d154f0f9
HomePC <jimdac@naver.com>
2022-02-22 01:26:34 +0900

    2022-02-22 dfs 정리 1

--- DFS-야옹님 강의/dfs 템플릿 정리.py
+++ DFS-야옹님 강의/dfs 템플릿 정리.py
@@ -75,5 +75,55 @@ def recur3(depth, start):
 
 recur3(0, 1)
 
-# 4번 템플릿(조합, 3번 템플릿보다 직관적, 2차원 dfs)
-def recur4(depth,start):pass
+# 백준 2961(https://www.acmicpc.net/problem/2961)
+ans = 1 << 60
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+
+# 4번 템플릿(조합, 3번 템플릿보다 직관적, 2차원 리스트의 dfs를 위한 템플릿)
+def recur4(depth, cnt, a, b):
+    global ans
+
+    if cnt == len_:  # 혹은 cnt != 0 으로 밑에서 반복문 안돌리고도 가능
+        ans = min(ans, abs(a - b))
+        return
+
+    if depth == n:
+        return
+
+    recur4(depth + 1, cnt + 1, a * arr[depth][0], b * arr[depth][1])
+    recur4(depth + 1, cnt, a, b)
+
+
+# 1개 부터 n 개까지 뽑는다.
+for i in range(1, n + 1):
+    len_ = i
+    recur4(0, 0, 1, 0)
+
+
+# 4번 템플릿2(조합, 3번 템플릿보다 직관적, 2차원 리스트의 dfs를 위한 템플릿)
+# 백준 1941(https://www.acmicpc.net/problem/1941)
+def recur4_2(r, c, cnt, scnt):
+    global ans
+
+    if cnt == 7:  # 혹은 cnt != 0 으로 밑에서 반복문 안돌리고도 가능
+        if scnt < 4:
+            return
+
+    if c == 5:  # 열이 이중리스트의 범위를 벗어난다면
+        r += 1  # 다음 행의
+        c = 0  # 첫열로 초기화
+        return
+
+    if r == 5:  # 만약 조건의 행의 범위를 벗어난다면 종료
+        return
+
+    recur4_2(r, c + 1, cnt + 1, scnt + (arr[r][c] == 'S'))
+    recur4_2(r, c + 1, cnt, scnt)
+
+
+# 1개 부터 n 개까지 뽑는다.
+for i in range(1, n + 1):
+    len_ = i
+    recur4(0, 0, 1, 0)
