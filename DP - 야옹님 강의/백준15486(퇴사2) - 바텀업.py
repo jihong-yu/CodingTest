b020beb64327456b02c230e3ad780b0e73b4bbfd
HomePC <jimdac@naver.com>
2022-04-04 13:31:09 +0900

    2022-04-04 수업시간(그래프,다익스트라)

--- DP - 야옹님 강의/백준15486(퇴사2) - 바텀업.py
+++ DP - 야옹님 강의/백준15486(퇴사2) - 바텀업.py
@@ -6,13 +6,21 @@ N = int(input())
 arr = [[0, 0]] + [list(map(int, input().split())) for _ in range(N)]
 dp = [0] * (N + 2)
 
+# 찾은 점화식
+# dp[cur] = max(dfs_2(cur + arr[cur][0]) + arr[cur][1], dfs_2(cur + 1))
 
-#dp[cur] = max(dfs_2(cur + arr[cur][0]) + arr[cur][1], dfs_2(cur + 1))
+# for i in range(1, N + 1):
+#     if arr[i][0] <= (N + 1) - i:
+#         dp[i + arr[i][0]] = max(dp[i + arr[i][0]], dp[i] + arr[i][1])
+#
+#     dp[i + 1] = max(dp[i + 1], dp[i])
+#
+# print(dp[-1])
 
-for i in range(1, N + 1):
-    if arr[i][0] <= (N + 1) - i:
-        dp[i + arr[i][0]] = max(dp[i + arr[i][0]], dp[i] + arr[i][1])
+for i in range(1, N + 1)[::-1]:
+    if i + arr[i][0] <= N + 1:
+        dp[i] = max(dp[i + arr[i][0]] + arr[i][1], dp[i + 1])
+    else:
+        dp[i] = dp[i + 1]
 
-    dp[i + 1] = max(dp[i + 1], dp[i])
-
-print(dp[-1])
+print(dp[1])
--- DP - 야옹님 강의/백준15988(1,2,3 더하기3).py
+++ DP - 야옹님 강의/백준15988(1,2,3 더하기3).py
@@ -1,31 +1,35 @@
 import sys
 
-sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
 
 
 def recur(total):
     ret = 0
-
+    print(total)
     if total > N:
         return 0
 
     if total == N:
         return 1
 
-    if dp[total] != -1:
-        return dp[total] % 1000000009
+    if dp.get(total, -1) != -1:
+        return dp[total]
 
     for i in range(1, 3 + 1):
-        ret += recur(total + i)
+        ret = recur(total + i)
 
-    dp[total] = ret % 1000000009
+    dp[total] = ret
 
-    return dp[total] % 1000000009
+    return dp[total]
 
+
+dp = {}
+
+dp[1], dp[2], dp[3] = 1, 2, 4
+for i in range(4, 1000001):
+    dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009
 
 T = int(input())
 for _ in range(T):
     N = int(input())
-    dp = [-1] * (N + 1)
-    print(recur(0))
+    print(dp[N])
