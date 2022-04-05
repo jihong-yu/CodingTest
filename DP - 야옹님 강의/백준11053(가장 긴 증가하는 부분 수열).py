d7ad6efc1c3a7305c68523eb2db5520e7925cc7b
HomePC <jimdac@naver.com>
2022-04-06 00:02:32 +0900

    2022-04-05 야옹 DP 5일차

---
+++ DP - 야옹님 강의/백준11053(가장 긴 증가하는 부분 수열).py
@@ -0,0 +1,28 @@
+import sys
+
+sys.setrecursionlimit(100000)
+
+N = int(input())
+A = list(map(int, input().split())) + [0]
+
+dp = [[-1 for _ in range(N + 1)] for _ in range(N + 1)]
+
+
+def dfs(cur, pre):
+    if cur == N:
+        return 0
+
+    if dp[cur][pre] != - 1:
+        return dp[cur][pre]
+
+    if A[cur] > A[pre]:
+        ret = max(dfs(cur + 1, cur) + 1, dfs(cur + 1, pre))
+    else:
+        ret = dfs(cur + 1, pre)
+
+    dp[cur][pre] = ret
+
+    return dp[cur][pre]
+
+
+print(dfs(0, -1))
