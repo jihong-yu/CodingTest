37331331314a31338e9d9fd43bcb9bcfb9290b6c
HomePC <jimdac@naver.com>
2022-04-15 00:09:53 +0900

    2022-04-14 야옹 DP 13일차

--- DP - 야옹님 강의/s1.py
+++ DP - 야옹님 강의/s1.py
@@ -4,3 +4,30 @@
 ^ 특정 비트 상태를 반전시킬 때 사용
 tip) x & (-x) x의 제일 오른쪽 1이 나온다.
 '''
+
+
+def DFS(li, cnt):
+    if cnt == M:
+        res.add(li)
+        return
+    for i in range(len(arr)):
+        if visited[i] == 0:
+            visited[i] = 1
+            DFS(li + ' ' + str(arr[i]), cnt + 1)
+            visited[i] = 0
+
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+res = set()
+
+visited = [0] * N
+DFS('', 0)
+solution = [[] for _ in range(len(res))]
+print(res)
+for idx, val in enumerate(res):
+    solution[idx] = list(map(int,val.split()))
+
+solution.sort()
+print(solution)
