ddfc4402f1d3080c1f5420a035e9aac8fa0a76d4
HomePC <jimdac@naver.com>
2022-04-17 01:14:15 +0900

    2022-04-16 야옹 DP 14일차

--- DP - 야옹님 강의/s1.py
+++ DP - 야옹님 강의/s1.py
@@ -1,33 +1,31 @@
-'''
-| 특정 비트를 킬때 사용
-& 특정비트가 켜저있는지 혹은 특정 비트를 끌 때 사용 (x & (1 << 2)) != 0 or (x & ~(1 << @))
-^ 특정 비트 상태를 반전시킬 때 사용
-tip) x & (-x) x의 제일 오른쪽 1이 나온다.
-'''
+N = int(input())
+city = [list(map(int, input().split())) for _ in range(N)]
+visited = [[-1 for _ in range(1 << N)] for _ in range(N)]
 
 
-def DFS(li, cnt):
-    if cnt == M:
-        res.add(li)
-        return
-    for i in range(len(arr)):
-        if visited[i] == 0:
-            visited[i] = 1
-            DFS(li + ' ' + str(arr[i]), cnt + 1)
-            visited[i] = 0
+def dfs(row, visit, start, cnt):
+    if cnt == N:
+        return 0
+
+    if visited[row][visit] != -1:
+        return visited[row][visit]
+
+    ret = 10000000
+    for i in range(N):
+        if visit & (1 << i) != 0 or city[row][i] == 0:  # 해당 노드를 이미 방문했거나 연결되어있지 않다면
+            continue
+        # 만약 마지막 방문 도시인데 처음도시를 선택하지 않았거나 현재 마지막 방문 도시가 아닌데 처음도시를 선택한 경우는 continue
+        if (cnt == N - 1 and i != start) or (cnt != N - 1 and i == start):
+            continue
+
+        ret = min(ret, dfs(i, visit | (1 << i), start, cnt + 1) + city[row][i])
+
+    visited[row][visit] = ret
+
+    return visited[row][visit]
 
 
-N, M = map(int, input().split())
-arr = list(map(int, input().split()))
-arr.sort()
-res = set()
+ans = 1 << 30
+ans = min(ans, dfs(0, 0, 0, 0))
 
-visited = [0] * N
-DFS('', 0)
-solution = [[] for _ in range(len(res))]
-print(res)
-for idx, val in enumerate(res):
-    solution[idx] = list(map(int,val.split()))
-
-solution.sort()
-print(solution)
+print(ans)
