f618581ed9501d717e38d56655aca9f9489f7cbb
HomePC <jimdac@naver.com>
2022-04-21 21:46:00 +0900

    2022-04-21 야옹 다익스트라 3일차

--- 다익스트라+유니온파인드 - 야옹스쿨/s1.py
+++ 다익스트라+유니온파인드 - 야옹스쿨/s1.py
@@ -1,6 +1,35 @@
-trace = {1:1}
+import sys
+from collections import deque
 
-n_trace = trace.copy()
-n_trace[2] = 1
-print(trace)
-print(n_trace)
+input = sys.stdin.readline
+
+
+def bfs(s):
+    global ans
+    visited[s] = True
+    que.append(s)
+
+    while que:
+        val = que.popleft()
+        for nxt in lst[val]:
+            if not visited[nxt]:
+                visited[nxt] = True
+                que.append(nxt)
+
+    ans += 1
+
+
+N, M = map(int, input().split())
+lst = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    lst[a].append(b)
+    lst[b].append(a)
+visited = [False for _ in range(N + 1)]
+que = deque()
+ans = 0
+for i in range(1, N + 1):
+    if not visited[i]:
+        bfs(i)
+
+print(ans)
