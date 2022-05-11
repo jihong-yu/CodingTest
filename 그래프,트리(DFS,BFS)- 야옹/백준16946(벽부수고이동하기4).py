f789c80a0a6e946af5ae40205761b682f64c621d
HomePC <jimdac@naver.com>
2022-05-11 23:53:05 +0900

    2022-05-11 클래스5 밀기 2일차

--- 그래프,트리(DFS,BFS)- 야옹/백준16946(벽부수고이동하기4).py
+++ 그래프,트리(DFS,BFS)- 야옹/백준16946(벽부수고이동하기4).py
@@ -2,36 +2,62 @@ from collections import deque
 
 
 def bfs(start_r, start_c):
-    visited = [[False] * M for _ in range(N)]
     queue = deque([[start_r, start_c]])
-    cnt = 1
-    visited[start_r][start_c] = True
+
+    visited[start_r][start_c] = group_num
+    group_cnt_dict[group_num] = 1
     while queue:
         r, c = queue.popleft()
-        if arr[r][c] == 0:
-            cnt += 1
 
         for i in range(4):
             nr = r + dr[i]
             nc = c + dc[i]
 
-            if nr < 0 or nr >= N or nc < 0 or nc >= M or arr[nr][nc] >= 1 or visited[nr][nc]:
+            if nr < 0 or nr >= N or nc < 0 or nc >= M or visited[nr][nc] != 0 or arr[nr][nc] == 1:
                 continue
-            visited[nr][nc] = True
+            visited[nr][nc] = group_num
+            group_cnt_dict[group_num] += 1
             queue.append([nr, nc])
 
-    arr[start_r][start_c] = cnt
+
+def bfs_2(start_r, start_c):
+    queue = deque([[start_r, start_c]])
+
+    while queue:
+        r, c = queue.popleft()
+
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            if nr < 0 or nr >= N or nc < 0 or nc >= M or arr[nr][nc] == 1 or not visited[nr][nc]:
+                continue
+
+            if not group_check.get(visited[nr][nc], 0):
+                arr[start_r][start_c] += group_cnt_dict.get(visited[nr][nc], 0)
+                arr[start_r][start_c] %= 10
+                group_check[visited[nr][nc]] = 1
 
 
 N, M = map(int, input().split())
 arr = [list(map(int, input())) for _ in range(N)]
 dr = [-1, 0, 1, 0]  # 상 우 하 좌
 dc = [0, 1, 0, -1]  # 상 우 하 좌
-loc = []
+group_num = 1
+group_cnt_dict = {}
+
+visited = [[0] * M for _ in range(N)]
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 0 and visited[i][j] == 0:
+            bfs(i, j)
+            group_num += 1
+
 for i in range(N):
     for j in range(M):
         if arr[i][j] == 1:
-            bfs(i, j)
+            group_check = {}
+            bfs_2(i, j)
 
 for i in range(N):
     for j in range(M):
